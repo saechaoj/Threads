78870ed80b9c1e2e94836a72e120385bcf0174bc
Jarrod Saechao <saechaoj@oregonstate.edu>
2020-11-18 23:50:25 -0800

    "new"

--- line_process.c
+++ line_process.c
@@ -9,36 +9,38 @@
 
 
 
-//#define CH 1000;
-//#define LI 50;
-#define END_MARKER -1;
+
 
 
 void* get_input(void*);
-void* put_buff_1(char*);
+void* put_buff_1(char);
 char* get_user_input();
 
 
 void* lineSeperator(void*);
 char* do_line(char*);
-char* get_buf_1();
-void* put_buff_2(char*);
+char get_buf_1();
+void* put_buff_2(char);
 
 
 void* plus(void*);
-char* get_buff_2();
+char get_buff_2();
 char* do_plus(char*);
-void* put_buff_3(char*);
+void* put_buff_3(char);
 
 
-char* get_buff_3();
+char get_buff_3();
 void* store_output(void*);
+char* check_stop(char*);
 
-char buffer[50000];
+
 char buffer_1[50000];
 char buffer_2[50000];
 char buffer_3[50000];
 int globalVar = 0;
+
+
+
 
 
 
@@ -70,8 +72,9 @@ pthread_cond_t full_3 = PTHREAD_COND_INITIALIZER;
 
 
 int main(int argc, char* argv[])
-{
-  
+{	
+	
+
 	pthread_t input_t, linSep_t, plus_t, output_t;
 
 	pthread_create(&input_t, NULL, get_input, NULL);
@@ -91,18 +94,65 @@ int main(int argc, char* argv[])
 
 
 
+//changes string if there is a stop statement then raises a global var for terminaltion of threads
+char* check_stop(char* x)
+{
+
+	char stop[7];
+	char* ptr = &stop[0];
+	sprintf(ptr,"STOP\n");
+
+	char* stop_ptr = strstr(x,ptr);
+
+
+	if(strcmp(ptr,stop_ptr) == 0)
+	{
+		globalVar = -1;
+		char new_string[1000];
+		strncpy(new_string,x,sizeof(x-ptr));
+		printf("%s",new_string);
+		return NULL;
+	}
+
+	else
+	{
+		return x;
+	}
+
+}
 
 
 
-// 4th thread
+
+
+// 4th thread handles the output and keeping count of the number of lines to print
 
 void* store_output(void* x)
-{	
-
+{
+	
+	int i = 0;
+	int j = 0;
+	char buf [50000];
+	char item = 0;
+	
 	while(1)
-	{	
-		get_buff_3();
-	}	
+	{
+		item = get_buff_3();
+		if(item == 1)
+		{
+			break;	
+		}
+		buf[i] = item;
+		i++;
+		if(i == 81)
+		{
+			printf("%s\n", buf);
+			i = 0;
+		}
+	}
+	
+	exit(1);
+
 }
 
 
@@ -114,33 +164,21 @@ void* store_output(void* x)
 
 
 // waits for third buffer then checks for 80 chars in that buffer to be printed
-char* get_buff_3()
+char get_buff_3()
 {
 
-
+	
 	pthread_mutex_lock(&mutex_3);
 	while(count_3 == 0)
 	{
-		 pthread_cond_wait(&full_3, &mutex_3);
-
+		pthread_cond_wait(&full_3, &mutex_3);
 	}
-
-	char printer [80];
-	char* temp = &buffer_3[con_idx_3];
-	if(strlen(buffer_3) - con_idx_3 > 80)
-	{
-
-		strncpy(printer, temp,80);
-		con_idx_3 = con_idx_3 + strlen(printer);
-		printf("Print Statement:%s\n", printer, strlen(printer));
-	
-	}
+	char item = buffer_3[con_idx_3];
+	con_idx_3++;
 	count_3--;
-
-
 	pthread_mutex_unlock(&mutex_3);
-	return buffer_3;
 
+	return item;
 
 }
 
@@ -149,20 +187,17 @@ char* get_buff_3()
 
 
 // sets buffer 3 value
-void* put_buff_3(char* x)
+void* put_buff_3(char x)
 {
 	
 	pthread_mutex_lock(&mutex_3);
-	char* temp = &buffer_3[prod_idx_3];
-	strcpy(temp,x);
-  	prod_idx_3 = strlen(buffer_3+1);
-	
-	if(prod_idx_3 - con_idx_3 > 80)
-	{	
-		count_3 = prod_idx_3 / 80;
-  		pthread_cond_signal(&full_3);
-	}
-    		pthread_mutex_unlock(&mutex_3);
+	buffer_3[prod_idx_3] = x;
+  	prod_idx_3++;
+	count_3++;
+  	pthread_cond_signal(&full_3);
+	pthread_mutex_unlock(&mutex_3);
+
+
 
 }
 
@@ -179,7 +214,7 @@ char* do_plus(char* x)
 {
 	
 
-	char temp_buffer[1000] = {0};
+	char temp_buffer[50000] = {0};
 	char* substring = "++";
 	char* replacement_string = "^";
 	char* insert_point = &temp_buffer[0];
@@ -221,13 +256,45 @@ char* do_plus(char* x)
 
 
 
-//Third thread 
+//Third thread grabs 2 char
 void* plus(void* x)
 {
-
-	while(1)
+	char newChar = '^';
+	char pluss = '+';
+	char item = 0;
+	char item2 = 0;
+	char item3 = 0;
+	for(int i = 0; i < 50000; i++)
 	{
-		put_buff_3(get_buff_2());
+		
+		item = get_buff_2();
+		
+		if(item == 1)
+		{
+			put_buff_3(1);
+			break;
+		}
+		item2 = get_buff_2();
+
+		if(item3 == item)
+		{
+			item = newChar;
+			put_buff_3(item3);
+		}
+		
+		else if(item == pluss && item2 == pluss)
+		{
+			item = newChar;
+			put_buff_3(item);
+			item3 = item2;
+		}
+		
+		
+			
+			put_buff_3(item);
+			put_buff_3(item2);
+			
+		
 	}	
 	return NULL;
 }
@@ -238,7 +305,7 @@ void* plus(void* x)
 
 
 //waits for line seperator 
-char* get_buff_2()
+char get_buff_2()
 {
 	pthread_mutex_lock(&mutex_2);
 	while(count_2 == 0)
@@ -247,12 +314,11 @@ char* get_buff_2()
 
 	}
 
+	char item = buffer_2[con_idx_2];
+	con_idx_2++;
 	count_2--;
-	char* temp = &buffer_2[con_idx_2];
-	con_idx_2 = prod_idx_2; 	
-	do_plus(temp);
 	pthread_mutex_unlock(&mutex_2);
-	return temp;
+	return item;
 
 	
 	
@@ -262,14 +328,13 @@ char* get_buff_2()
 
 
 
-//adds string to second buffer
-void* put_buff_2(char* x)
+//adds char to second buffer
+void* put_buff_2(char x)
 {
 	
 	pthread_mutex_lock(&mutex_2);
-	char* temp = &buffer_2[prod_idx_2];
-	strcpy(temp,x);
-     	prod_idx_2 = strlen(buffer_2+1);
+	buffer_2[prod_idx_2] = x;
+     	prod_idx_2++;
 	count_2++;
   	pthread_cond_signal(&full_2);
    	pthread_mutex_unlock(&mutex_2);
@@ -280,8 +345,8 @@ void* put_buff_2(char* x)
 
 
 
-//waits for first thread then accesses memory to swap \n then returns line
-char* get_buff_1()
+//waits for first thread then accesses memory to grab buffer char
+char get_buff_1()
 {
 	pthread_mutex_lock(&mutex_1);
   	while (count_1 == 0)
@@ -292,31 +357,46 @@ char* get_buff_1()
 	 }
 
 
-	char* temp;
+	char item = buffer_1[con_idx_1];
+	con_idx_1++;
 	count_1--;
-	 temp = &buffer_1[con_idx_1];
-
-
-	buffer_1[prod_idx_1] = '~';
-	con_idx_1 = prod_idx_1++;
 	pthread_mutex_unlock(&mutex_1);
 	
-	return temp;
+	return item;
       
 }
 
 
 
 
-//Second thread
+//Second thread grabs from buffer 1 then checks for null or STOP call
 void* lineSeperator(void*x)
 {
-	while(1)
-	{
-		put_buff_2(get_buff_1());
 
-	}
+	char item = 0;
+	char newChar = ' ';
+	
+	for(int i = 0; i < 500000; i++)
+	{
+		 item = get_buff_1();
+	
+		if( item == 1)
+		{
+		  put_buff_2(1);
+		  break;
+		}
+		
+		if(item =='\n')
+		{
+			item = newChar;
+
+		}
+
+		put_buff_2(item);
+	}		
+	
 	return NULL;
+		
  }
 
 
@@ -326,12 +406,19 @@ void* lineSeperator(void*x)
 
 
 
+
+
+
+
+
+
+// NOT USED
 //reads in user input returns it to input thread function
 char* get_user_input()
 {
 
-    char* user_input = malloc(sizeof(char)*50000);
-	fgets(user_input,50000,stdin);
+    char* user_input = calloc(1000,sizeof(char));
+	fgets(user_input,1000,stdin);
 	fflush(stdin);
 
     return user_input;
@@ -340,13 +427,30 @@ char* get_user_input()
 
 
 
+
+
+
+
+
+
 //Takes in user input and puts it in buffer for first thread
 void *get_input(void* x)
 {
-   while(1)
+ 
+	   char* buffer = calloc(500000,sizeof(char));
+	while(fgets(buffer,1000,stdin) != NULL && strcmp(buffer,"STOP\n"))
 	{
-	     put_buff_1(get_user_input());
-	}	   
+
+		for(int i = 0 ; i < strlen(buffer); i++)
+		{
+				put_buff_1(buffer[i]);
+		}
+	
+	}
+	 
+	put_buff_1(1);
+
+		
       return NULL;
 
 }
@@ -357,20 +461,24 @@ void *get_input(void* x)
 
 
 // reads in user input and adds it to input buffer
-void* put_buff_1(char* x)
+void* put_buff_1(char x)
 {
 // //  Lock the mutex before putting the item in the buffer
    pthread_mutex_lock(&mutex_1);
 //   // Put the item in the buffer
   
-   char* temp = &buffer_1[prod_idx_1];
-   strcpy(temp,x);	
+//   char* temp = &buffer_1[prod_idx_1];
+
+   buffer_1[prod_idx_1] = x;
+
+//	check_stop(temp);	
 //   // Increment the index here the next item will be put.
-   prod_idx_1 = strlen(buffer_1+1);
+  // prod_idx_1 = strlen(buffer_1+1);
+  	prod_idx_1++;
 	count_1++;
 //   // Signal to the consumer that the buffer is no longer empty
    pthread_cond_signal(&full_1);
 //   // Unlock the mutex
    pthread_mutex_unlock(&mutex_1);
-	free(x);
+
 }
